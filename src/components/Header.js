6b8d574da1d375b7bfc0c63983d6db0edacd559b
bilaloum <bilaloumehdi01@gmail.com>
2022-12-08 10:57:49 +0100

    header-component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>projects</li>
+                    <li>about</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
