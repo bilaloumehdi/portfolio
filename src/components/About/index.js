cb413abe7bcd5de6f10ff9491c218ce9e1d2ec5d
bilaloum <bilaloumehdi01@gmail.com>
2023-01-12 23:42:22 +0100

    update about-page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import './index.scss';
 import { useEffect, useState } from "react";
 import  AnimatedLatters  from "../AnimatedLatters";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
 const About = () => {
         const [letterClass, setLetterClass] = useState('text-animate');
 
@@ -33,6 +35,31 @@ const About = () => {
                     quidem 
                     labore amet ipsam, iure sint officia?</p>
             </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faReact}  color="#5ED4F4" />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faNodeJs} color="#3c873a" />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faHtml5}  color="#F06529" />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faCss3}  color="#28A4D9" />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faJsSquare}  color="#EFD81D" />
+                    </div>
+                    <div className="face7">
+                        <FontAwesomeIcon icon={faGitAlt}  color="#DD0031" />
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
