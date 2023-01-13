5d1cda7cd98c27bab5f5b99e8f71efacc0f7c8a8
Bilal Oumehdi <95442422+Bilaloum@users.noreply.github.com>
2023-01-13 22:26:35 +0100

    Delete env.js

--- public/env.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  "REACT_APP_EMAILJS_PUBLIC_KEY": "Oer5PpEWXUiKYnLbF",
-  "REACT_APP_EMAILJS_SERVICE_ID": "service_p93rjgx",
-  "REACT_APP_EMAILJS_TEMPLATE_ID": "template_dpzmy5i"
-};
